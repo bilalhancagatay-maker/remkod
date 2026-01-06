60ac0eebbf347602d1589b353035a2567b3dfb01
bilalhancagatay-maker <bilalhancagatay@gmail.com>
2026-01-06 13:05:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Vercel'deki process.env değişkenlerini tarayıcı koduna aktarır
+    'process.env': process.env
+  },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    target: 'esnext'
   },
   server: {
     port: 3000
